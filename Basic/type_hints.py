a5e84c75fc13f04b579ae49da1c593eaccca1a7e
iamlucasmateo <perrone.lucasmateo@gmail.com>
2022-03-18 12:50:56 -0300

    endpoints and body validations examples

--- Basic/type_hints.py
+++ Basic/type_hints.py
@@ -1,3 +1,5 @@
+import typing
+
 def greet(name: str, last_name):
     # method suggestions from the IDE come from the type hinting
     greeting = f"Hello, {name.capitalize()} {last_name.capitalize()}"  
@@ -6,3 +8,9 @@ def greet(name: str, last_name):
 def greet_age(name: str, age: int):
     # mypy will indicate this is a problem
     greeting = f"Hello {name.capitalize()}, your age is " + age
+    return greeting
+
+def dict_params(my_dict: typing.Dict[str, int]):
+    for k, v in my_dict.items():
+        print(k.capitalize())
+        print(v.as_integer_ratio())
